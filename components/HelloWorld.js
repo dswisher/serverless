eb4261b4f3155202a0a670775a4595ce49682a78
Doug Swisher <big.swish@gmail.com>
2017-11-26 19:50:19 -0600

    Add templates and components directories

---
+++ components/HelloWorld.js
@@ -0,0 +1,5 @@
+
+Vue.component('hello-world', {
+    template: templates.HelloWorld,
+});
+
--- gulpfile.js
+++ gulpfile.js
@@ -2,17 +2,59 @@
 var gulp = require('gulp');
 var del = require('del');
 var webserver = require('gulp-webserver');
+var sourcemaps = require('gulp-sourcemaps');
+var jshint = require('gulp-jshint');
+var concat = require('gulp-concat');
+var filesToKeys = require('gulp-file-contents-to-keys');
 
 
-gulp.task('html', function() {
+var paths = {
+    'copy:html': ['./pages/*.html'],
+    'package:templates': ['./templates/*.html'],
+    'compile:js': ['./tmp/templates.js', './components/*.js']
+}
+
+
+gulp.task('copy:html', function() {
     return gulp
-        .src('./html/*.html')
+        .src(paths['copy:html'])
         // TODO - htmlhint? htmlmin?
         .pipe(gulp.dest('./dist'));
 });
 
 
-gulp.task('vendorjs', function() {
+gulp.task('copy:css', function() {
+    return gulp
+        .src('./node_modules/bulma/css/bulma.css')
+        .pipe(gulp.dest('./dist/css'));
+});
+
+
+gulp.task('package:templates', function() {
+    return gulp
+        .src(paths['package:templates'])
+        .pipe(filesToKeys({
+            fileName: 'templates.js'
+        }))
+        .pipe(gulp.dest('./tmp'));
+});
+
+
+gulp.task('compile:js', ['package:templates'], function() {
+    return gulp
+        .src(paths['compile:js'])
+        .pipe(sourcemaps.init())
+        .pipe(jshint({
+            'esversion': 6
+        }))
+        .pipe(jshint.reporter('default'))
+        .pipe(concat('app.js'))
+        .pipe(sourcemaps.write('../maps'))
+        .pipe(gulp.dest('dist/js'));
+});
+
+
+gulp.task('copy:js', ['compile:js'], function() {
     return gulp
         // TODO - pick up more JS files
         .src('./node_modules/vue/dist/vue.js')
@@ -22,13 +64,22 @@ gulp.task('vendorjs', function() {
 
 
 gulp.task('clean', function() {
-  return del(['dist']);
+  return del(['dist', 'tmp']);
 });
 
 
-// TODO - add watch target, and have serve depend on it
+gulp.task('watch', ['copy:html', 'copy:css', 'copy:js'], function() {
+    for (key in paths) {
+        console.log('Watching: ' + key + ' files...');
+        gulp.watch(paths[key], [key])
+            .on('change', function(event) {
+                console.log(`Watch: ${event.path} was ${event.type}.`);
+            });
+    }
+});
 
-gulp.task('serve', ['html'], function() {
+
+gulp.task('serve', ['watch'], function() {
   gulp.src('dist')
     .pipe(webserver({
       directoryListing: false,
